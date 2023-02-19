171a43c063dfec790a751ebd33b316670a6d5c56
Brijen Makwana <brijenmakwana@192.168.1.4>
2023-02-19 16:56:15 +0530

    yuoyube videos added to ShowScreen

---
+++ components/YouTubeCard.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import { WebView } from "react-native-webview";
+
+const YouTubeCard = (props) => {
+  const { videoKey } = props;
+  return (
+    <WebView
+      style={styles.container}
+      source={{
+        uri: `https://www.youtube.com/embed/${videoKey}?rel=0&autoplay=0&showinfo=0&controls=0`,
+      }}
+    />
+  );
+};
+
+export default YouTubeCard;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 120,
+    width: 230,
+    marginRight: 7,
+  },
+});
